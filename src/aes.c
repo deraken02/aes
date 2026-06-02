aa827fe2cbce1aefad1409301e0f6b6b87312264
deraken02 <louisdelacroix3@gmail.com>
2026-06-02 20:13:23 +0200

    Cypher and uncypher files

--- src/aes.c
+++ src/aes.c
@@ -1,28 +1,32 @@
 #include <cypher.h>
 #include <decypher.h>
 
+#include <fcntl.h>
 #include <stdbool.h>
 #include <stdio.h>
 
-#if defined(SHA256_KEY_LEY)
-#define SHA_LEN (256/8)
-#define EXPEND_KEY_WORDS_NB 60
-#define ROUND_KEY_EXPANSION (256/32)
-#define NB_OF_ROUND 14
-#else
-#define SHA_LEN (128/8)
-#define EXPEND_KEY_WORDS_NB 44
-#define ROUND_KEY_EXPANSION (128/32)
-#define NB_OF_ROUND 10
-#endif
+#define SHA_LEN_256 (256/8)
+#define EXPEND_KEY_WORDS_NB_256  60
+#define ROUND_KEY_EXPANSION_256  (256/32)
+#define NB_OF_ROUND_256  14
 
+#define SHA_LEN_128 (128/8)
+#define EXPEND_KEY_WORDS_NB_128 44
+#define ROUND_KEY_EXPANSION_128 (128/32)
+#define NB_OF_ROUND_128 10
 
-/**
- * Error code:
- *  -  0 : No errors
- *  - -1 : Error from sha256 generation
- */
-int32_t generate_256bits_key(char *keypass, uint8_t out_key[SHA_LEN])
+typedef struct cypher_block_arg_t
+{
+    uint8_t block[16];
+    uint32_t expended_key[EXPEND_KEY_WORDS_NB_256];
+} cypher_block_arg_st; 
+
+static uint32_t SHA_LEN = SHA_LEN_128;
+static uint32_t EXPEND_KEY_WORDS_NB = EXPEND_KEY_WORDS_NB_128;
+static uint32_t ROUND_KEY_EXPANSION = ROUND_KEY_EXPANSION_128;
+static uint32_t NB_OF_ROUND = NB_OF_ROUND_128;
+
+static int32_t generate_256bits_key(char *keypass, uint8_t out_key[SHA_LEN])
 {
     int32_t sta = 0;
     if(SHA256((const uint8_t *)keypass, strlen(keypass), out_key) == NULL)
@@ -78,12 +82,6 @@ void print_matrix(uint8_t matrix[16])
     putchar('\n');
 }
 
-typedef struct cypher_block_arg_t
-{
-    uint8_t block[16];
-    uint32_t expended_key[EXPEND_KEY_WORDS_NB];
-} cypher_block_arg_st; 
-
 void *cypher_block(void *argument)
 {
     cypher_block_arg_st *arg = (cypher_block_arg_st *)argument;
@@ -118,22 +116,135 @@ void *decypher_block(void *argument)
     return argument;
 }
 
+int32_t compute(cypher_block_arg_st arg,int32_t io_fd[2],bool decypher, int32_t thread_nb)
+{
+    int32_t sta = 0;
+    uint8_t readed = read(io_fd[0], arg.block, 16);
+    while( readed == 16)
+    {
+        if(decypher != true)
+        {
+            cypher_block(&arg);
+        } else
+        {
+            decypher_block(&arg);
+        }
+        write(io_fd[1], arg.block, 16);
+        memset(&(arg.block), 0, 16);
+        readed = read(io_fd[0], arg.block, 16);
+    }
+    if(readed != 0)
+    {
+        if(decypher != true)
+        {
+            cypher_block(&arg);
+        } else
+        {
+            decypher_block(&arg);
+        }
+        write(io_fd[1], arg.block, 16);
+    }
+    return sta;
+}
 
 int32_t main(int32_t argc, char *argv[])
 {
-    uint8_t sha_key[SHA_LEN];
+    uint8_t sha_key[SHA_LEN_256] = {0};
+    char key_pass[256] = {0};
     int32_t sta = 0;
-    cypher_block_arg_st arg={
-        .block= {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}
-    };
-    if(argc != 2)
+    int32_t io_fd[2] = {STDIN_FILENO, STDOUT_FILENO};
+    bool password_found = false;
+    bool decypher = false;
+    int32_t thread_nb = 1;
+    cypher_block_arg_st arg;
+    memset(&arg, 0, sizeof(cypher_block_arg_st)); 
+    if(argc == 1)
     {
-        perror("Bad usage: need password in parameter");
-        return EBADMSG;
+        printf("Enter password: ");
+        fflush(NULL);
+        sta = read(STDIN_FILENO, key_pass, 256);
+        if(sta == 0)
+        {
+            perror("Empty password\n");
+            return -1;
+        } else
+        {
+            password_found = true;
+        }
+    } else
+    {
+        for(uint8_t i = 1; i<argc; i++)
+        {
+            if(strncmp(argv[i], "--passwd", 8) == 0)
+            {
+                i++;
+                strncpy(key_pass, argv[i], 256);
+                password_found = true;
+            } else if(strncmp(argv[i], "--file_in", 9) == 0)
+            {
+                if(argv[i][9] == '=')
+                {
+                    sta = open(&argv[i][10], O_RDONLY);
+                } else
+                {
+                    i++;
+                    sta = open(argv[i], O_RDONLY);
+                }
+                if (sta > 2)
+                {
+                    io_fd[0] = sta;
+                    sta = 0;
+                } else
+                {
+                    perror("Can't open file");
+                }
+            } else if(strncmp(argv[i], "--file_out", 10) == 0)
+            {
+                if(argv[i][10] == '=')
+                {
+                    sta = open(&argv[i][11], O_CREAT|O_WRONLY|O_TRUNC, 420);
+                } else
+                {
+                    i++;
+                    sta = open(argv[i], O_CREAT|O_WRONLY|O_TRUNC, 420);
+                }
+                if (sta > 2)
+                {
+                    io_fd[1] = sta;
+                    sta = 0;
+                } else
+                {
+                    perror("Can't open file");
+                }
+            } else if(strncmp(argv[i], "--thread", 8) == 0)
+            {
+                i++;
+                /* To be done */
+            } else if(strncmp(argv[i], "--cypher", 8) == 0)
+            {
+                decypher = false;
+            } else if(strncmp(argv[i], "--decypher", 10) == 0)
+            {
+                decypher = true;
+            } else
+            {
+                printf("Unknown options %s\n", argv[i]);
+            }
+        }
     }
-    memset(sha_key, 48, SHA_LEN);
-    memcpy(sha_key,argv[1], strlen(argv[1]));
-    //sta = generate_256bits_key(argv[1], sha_key);
+    if(password_found != true)
+    {
+        perror("A password is mandatory");
+        if(io_fd[0] != 0)
+        {
+            close(io_fd[0]);
+        }
+        if(io_fd[1] != 1) {
+            close(io_fd[1]);
+        }
+        return -1;
+    }
+    sta = generate_256bits_key(key_pass, sha_key);
     if(sta != 0)
     {
         perror("Something went wrong with OpenSSL");
@@ -141,13 +252,17 @@ int32_t main(int32_t argc, char *argv[])
     {
         sta = expand_key(sha_key, arg.expended_key);
     }
-    printf("Start\n");
-    print_matrix(arg.block);
-    (void)cypher_block((void *)&arg);
-    printf("Cyphered \n");
-    print_matrix(arg.block);
-    printf("Decypher \n");
-    (void)decypher_block((void *)&arg);
-    print_matrix(arg.block);
+
+    sta = compute(arg, io_fd, decypher, 1);    
+
+    if(io_fd[0] != 0)
+    {
+        close(io_fd[0]);
+    }
+    if(io_fd[1] != 1)
+    {
+        close(io_fd[1]);
+    }
     return sta;
 }
+
